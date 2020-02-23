0798d4bb7373e291a619ca74d7a0b03b863fb408
SirArchLinux <>
2020-02-23 15:16:52 +0100

    Manually merge TrontheTechie's new Animations

--- src/main/java/com/sirarchlinux/runelite/polybar/PolybarIntegrationPlugin.java
+++ src/main/java/com/sirarchlinux/runelite/polybar/PolybarIntegrationPlugin.java
@@ -75,6 +75,189 @@ public class PolybarIntegrationPlugin extends Plugin {
             case WOODCUTTING_CRYSTAL:
                 newState = State.WCING;
                 break;
+            case MINING_BRONZE_PICKAXE:
+            case MINING_IRON_PICKAXE:
+            case MINING_STEEL_PICKAXE:
+            case MINING_BLACK_PICKAXE:
+            case MINING_MITHRIL_PICKAXE:
+            case MINING_ADAMANT_PICKAXE:
+            case MINING_RUNE_PICKAXE:
+            case MINING_DRAGON_PICKAXE:
+            case MINING_DRAGON_PICKAXE_UPGRADED:
+            case MINING_DRAGON_PICKAXE_OR:
+            case MINING_INFERNAL_PICKAXE:
+            case MINING_3A_PICKAXE:
+            case MINING_CRYSTAL_PICKAXE:
+            case MINING_MOTHERLODE_BRONZE:
+            case MINING_MOTHERLODE_IRON:
+            case MINING_MOTHERLODE_STEEL:
+            case MINING_MOTHERLODE_BLACK:
+            case MINING_MOTHERLODE_MITHRIL:
+            case MINING_MOTHERLODE_ADAMANT:
+            case MINING_MOTHERLODE_RUNE:
+            case MINING_MOTHERLODE_DRAGON:
+            case MINING_MOTHERLODE_DRAGON_UPGRADED:
+            case MINING_MOTHERLODE_DRAGON_OR:
+            case MINING_MOTHERLODE_INFERNAL:
+            case MINING_MOTHERLODE_3A:
+            case MINING_MOTHERLODE_CRYSTAL:
+            case 643:
+                newState = State.MINING;
+                break;
+            case 713:
+            case 712:
+                newState = State.ALCH;
+                break;
+            case 833:
+            case 828:
+                newState = State.CLIMB;
+                break;
+            case SMITHING_ANVIL:
+                newState = State.SMITH;
+                break;
+            case SMITHING_SMELTING:
+            case SMITHING_CANNONBALL:
+                newState = State.SMELT;
+                break;
+            case 3705:
+                newState = State.PRAY;
+                break;
+            case FLETCHING_BOW_CUTTING:
+            case FLETCHING_ATTACH_FEATHERS_TO_ARROWSHAFT:
+            case FLETCHING_ATTACH_HEADS:
+            case FLETCHING_STRING_MAGIC_LONGBOW:
+            case FLETCHING_STRING_MAGIC_SHORTBOW:
+            case FLETCHING_STRING_MAPLE_LONGBOW:
+            case FLETCHING_STRING_MAPLE_SHORTBOW:
+            case FLETCHING_STRING_NORMAL_LONGBOW:
+            case FLETCHING_STRING_NORMAL_SHORTBOW:
+            case FLETCHING_STRING_OAK_LONGBOW:
+            case FLETCHING_STRING_OAK_SHORTBOW:
+            case FLETCHING_STRING_WILLOW_LONGBOW:
+            case FLETCHING_STRING_WILLOW_SHORTBOW:
+            case FLETCHING_STRING_YEW_LONGBOW:
+            case FLETCHING_STRING_YEW_SHORTBOW:
+                newState = State.FLETCH;
+                break;
+            case COOKING_FIRE:
+            case COOKING_RANGE:
+            case COOKING_WINE:
+                newState = State.COOK;
+                break;
+            case 397:
+                newState = State.STUN;
+                break;
+            case GEM_CUTTING_AMETHYST:
+            case GEM_CUTTING_DIAMOND:
+            case GEM_CUTTING_EMERALD:
+            case GEM_CUTTING_JADE:
+            case GEM_CUTTING_OPAL:
+            case GEM_CUTTING_REDTOPAZ:
+            case GEM_CUTTING_RUBY:
+            case GEM_CUTTING_SAPPHIRE:
+                newState = State.GEM;
+                break;
+            case FISHING_POLE_CAST:
+            case FISHING_BARBTAIL_HARPOON:
+            case FISHING_BAREHAND:
+            case FISHING_BIG_NET:
+            case FISHING_CAGE:
+            case FISHING_CRYSTAL_HARPOON:
+            case FISHING_DRAGON_HARPOON:
+            case FISHING_HARPOON:
+            case FISHING_INFERNAL_HARPOON:
+            case FISHING_KARAMBWAN:
+            case FISHING_NET:
+            case FISHING_OILY_ROD:
+                newState = State.FISH;
+                break;
+            case BOOK_HOME_TELEPORT_1:
+            case BOOK_HOME_TELEPORT_2:
+            case BOOK_HOME_TELEPORT_3:
+            case BOOK_HOME_TELEPORT_4:
+            case BOOK_HOME_TELEPORT_5:
+            case COW_HOME_TELEPORT_1:
+            case COW_HOME_TELEPORT_2:
+            case COW_HOME_TELEPORT_3:
+            case COW_HOME_TELEPORT_4:
+            case COW_HOME_TELEPORT_5:
+            case COW_HOME_TELEPORT_6:
+                newState = State.TELE;
+                break;
+            case FIREMAKING:
+                newState = State.FIRE;
+                break;
+            case 881:
+            case 832:
+                newState = State.STEAL;
+                break;
+            case 714:
+                newState = State.MTELE;
+                break;
+            case FARMING_HARVEST_ALLOTMENT:
+            case FARMING_HARVEST_BUSH:
+            case FARMING_HARVEST_FLOWER:
+            case FARMING_HARVEST_FRUIT_TREE:
+            case FARMING_HARVEST_HERB:
+            case FARMING_MIX_ULTRACOMPOST:
+            case FARMING_CURE_WITH_POTION:
+            case FARMING_PLANT_SEED:
+            case FARMING_USE_COMPOST:
+                newState = State.FARM;
+                break;
+            case HERBLORE_MAKE_TAR:
+            case HERBLORE_PESTLE_AND_MORTAR:
+            case HERBLORE_POTIONMAKING:
+                newState = State.HERB;
+                break;
+            case HUNTER_CHECK_BIRD_SNARE:
+            case HUNTER_CHECK_BOX_TRAP:
+                newState = State.CHUNT;
+                break;
+            case HUNTER_LAY_BOXTRAP_BIRDSNARE:
+            case HUNTER_LAY_MANIACAL_MONKEY_BOULDER_TRAP:
+            case HUNTER_LAY_NETTRAP:
+                newState = State.LHUNT;
+                break;
+            case SAND_COLLECTION:
+                newState = State.SAND;
+                break;
+            case MAGIC_MAKE_TABLET:
+            case HOME_MAKE_TABLET:
+                newState = State.TABMK;
+                break;
+            case CONSTRUCTION:
+                newState = State.CONSTR;
+                break;
+            case CRAFTING_BATTLESTAVES:
+                newState = State.CRFTBS;
+                break;
+            case CRAFTING_GLASSBLOWING:
+            case CRAFTING_LEATHER:
+            case CRAFTING_POTTERS_WHEEL:
+            case CRAFTING_POTTERY_OVEN:
+            case CRAFTING_SPINNING:
+                newState = State.CRAFT;
+                break;
+            case MAGIC_CHARGING_ORBS:
+                newState = State.CHORB;
+                break;
+            case MAGIC_ARCEUUS_RESURRECT_CROPS:
+            case MAGIC_LUNAR_CURE_PLANT:
+                newState = State.BCRPS;
+                break;
+            case MAGIC_LUNAR_PLANK_MAKE:
+                newState = State.PLANK;
+                break;
+            case MAGIC_LUNAR_STRING_JEWELRY:
+                newState = State.STRNG;
+                break;
+            case MAGIC_ENCHANTING_AMULET_1:
+            case MAGIC_ENCHANTING_AMULET_2:
+            case MAGIC_ENCHANTING_AMULET_3:
+            case MAGIC_ENCHANTING_JEWELRY:
+                newState = State.ENCHJWL;
+                break;
             default:
                 newState = State.IDLE;
                 break;
